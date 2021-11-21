37ca97e63109762a2612638658e2a91544e9ca6f
Vladyslav Ohirenko <aguretsvlad@gmail.com>
2021-11-21 14:57:28 +0200

    chore: rm comments

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,6 @@
  	import Header from './components/Header.svelte';
   import SourceInput from './components/SourceInput.svelte';
   import FormattedCode from './components/FormattedCode.svelte';
-
-  // TODO:
-  // 1. Add popup with manual
-  // 2. Configure CI/CD
-  // 3. Show code / variables label rotated on 90 deg
-  // 4. Syntax highlight
-  // 5. Parse input params (move each on next line)
 </script>
 
 <div class="header">
